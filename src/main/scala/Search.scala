40202a9b2660ae00cc20583bf2d47006579f8559
Yulia <prolomova2012@yandex.ru>
2018-12-06 18:53:54 +0500

    Доделан парсер

--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -15,6 +15,15 @@ object Search extends App {
       str
     }
 
+    def findTweetsByHashtag (hashtag: String): Unit = {
+      val link = "https://twitter.com/search?q=%23" + hashtag + "&src=typd&lang=ru"
+      val page = getPage(new URL(link))
+      println(page)
+      val regex = "tweet js-stream-tweet js-actionable-tweet js-profile-popup-actionable dismissible-content(.)".r
+      val links = regex.findAllIn(page)
+      println(links)
+    }
+
     def extract_data(page: String, url: String): Tweet = {
       val tweetId = url.split('/').toList.last
       val regex = ("(?s)<span class=\"ProfileTweet-actionCount\"\\s+data-tweet-stat-count=\"(\\d+).*?" + tweetId).r
@@ -25,7 +34,12 @@ object Search extends App {
         if (comments.hasNext)
           comments.next()
       }
-      new Tweet(data.head, data(1), data(2))
+      val regexContent = ("(?s)<link rel=\"preload\".*\\s+<title>(.+)" + "</title>").r
+      val content = regexContent.findAllIn(page)
+      val regexDate = (tweetId + "\" class=\"tweet-timestamp js-permalink js-nav js-tooltip\" title=\"(\\d{1,2}:\\d{1,2}) - (\\d{1,2}) (\\S{3,4}) (\\d{4})").r
+      val date = regexDate.findAllIn(page)
+      //println(date.group(4)) хз, как месяц определить
+      new Tweet(data.head, data(1), data(2), content.group(1))
     }
   }
 
@@ -33,10 +47,13 @@ object Search extends App {
     val Likes: Int
     val Retweets: Int
     val Comments: Int
+    val Content: String
+    val Date: Date
   }
 
-  sealed class Tweet(val Comments : Int, val Retweets: Int, val Likes: Int) extends TweetTrait
+  sealed class Tweet(val Comments : Int, val Retweets: Int, val Likes: Int, val Content: String)
 
-  print(Search.extract_data(Search.getPage(new URL("https://twitter.com/rihanna/status/1048481053067960321")),
-    "https://twitter.com/rihanna/status/1048481053067960321"))
+  Search.findTweetsByHashtag("cat")
+  //print(Search.extract_data(Search.getPage(new URL("https://twitter.com/rihanna/status/1048481053067960321")),
+    //"https://twitter.com/rihanna/status/1048481053067960321"))
 }
